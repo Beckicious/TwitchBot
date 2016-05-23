f405f957500f1597c2e527df60979cfb19fea68e
Simon Beck <simon.beck@students.fhnw.ch>
2016-05-23 09:38:59 +0200

    refactored BasicTwitchBot

--- TwitchBot/BasicTwitchBot.cs
+++ TwitchBot/BasicTwitchBot.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Net;
+﻿using System.Threading;
 using System.Net.Sockets;
 using System.IO;
 using System.Collections.Concurrent;
@@ -11,11 +6,35 @@ using TwitchBot.Commands;
 
 namespace TwitchBot
 {
+    /// <summary>
+    /// basic implementation of a twitch chat communicator
+    /// 
+    /// Multithreaded with 4 layers
+    /// 1.layer
+    ///     1 thread that constantly reads from the inputstream and writes it into the inputQueue
+    /// 
+    /// 2.layer
+    ///     number of threads (default 10) that take messages from the inputQueue and handle the 
+    ///     messages with HandleMessage and put the result into the commandQueue if needed
+    /// 
+    /// 3.layer
+    ///     number of threads (default 10) that take commands from the commandQueue and execute them
+    ///     if needed commands will write messages into the outputQueue
+    /// 
+    /// 4.layer
+    ///     1 thread that constantly takes messages from the outputQueue and writes them into the outputStream
+    /// </summary>
     public class BasicTwitchBot
     {
-
+        /// <summary>
+        /// userName of the twitchbot account
+        /// </summary>
         private readonly string userName;
-        private readonly string password;
+
+        /// <summary>
+        /// token of the twitchbot account (https://twitchapps.com/tmi/)
+        /// </summary>
+        private readonly string token;
         private string channelName = "";
 
         private TcpClient tcpClient;
@@ -29,16 +48,28 @@ namespace TwitchBot
         private readonly int messageHandlerCount;
         private readonly int commandHandlerCount;
 
-        public BasicTwitchBot(string userName, string password, int messageHandlerCount = 10, int commandHandlerCount = 10)
+        /// <summary>
+        /// constructor
+        /// creates the TwitchBot without connecting
+        /// </summary>
+        /// <param name="userName">userName of the twitchbot account</param>
+        /// <param name="token">token of the twitchbot account</param>
+        /// <param name="messageHandlerCount">number of layer 2 threads (default 10)</param>
+        /// <param name="commandHandlerCount">number of layer 3 threads (default 10)</param>
+        public BasicTwitchBot(string userName, string token, int messageHandlerCount = 10, int commandHandlerCount = 10)
         {
             this.userName = userName;
-            this.password = password;
+            this.token = token;
 
             this.messageHandlerCount = messageHandlerCount;
             this.commandHandlerCount = commandHandlerCount;
-
         }
 
+        /// <summary>
+        /// sets up the TcpClient and connects with twitch
+        /// starts the threads for all 4 layers
+        /// </summary>
+        /// <returns>true if everything went well, false if an exception was thrown</returns>
         public bool Connect()
         {
             try
@@ -52,7 +83,7 @@ namespace TwitchBot
                 outputQueue = new BlockingCollection<string>(new ConcurrentQueue<string>());
                 commandQueue = new BlockingCollection<Command>(new ConcurrentQueue<Command>());
 
-                outputStream.WriteLine("PASS " + password);
+                outputStream.WriteLine("PASS " + token);
                 outputStream.WriteLine("NICK " + userName);
                 outputStream.WriteLine("USER " + userName + " 8 * :" + userName);
                 outputStream.Flush();
@@ -68,9 +99,12 @@ namespace TwitchBot
 
         }
 
+        /// <summary>
+        /// creates and starts the threads of all 4 layers
+        /// </summary>
         public void startReadWriting()
         {
-
+            //layer 1 thread
             new Thread(() =>
             {
                 while (true)
@@ -83,6 +117,31 @@ namespace TwitchBot
                 }
             }).Start();
 
+            //layer 2 threads
+            for (int i = 0; i < messageHandlerCount; i++)
+            {
+                new Thread(() =>
+                {
+                    while(true)
+                    {
+                        HandleMessageIntern(inputQueue.Take());
+                    }
+                }).Start();
+            }
+
+            //layer 3 threads
+            for (int i = 0; i < commandHandlerCount; i++)
+            {
+                new Thread(() =>
+                {
+                    while (true)
+                    {
+                        commandQueue.Take().Execute();
+                    }
+                }).Start();
+            }
+
+            //layer 4 thread
             new Thread(() =>
             {
                 while (true)
@@ -95,30 +154,12 @@ namespace TwitchBot
                     catch { }
                 }
             }).Start();
-
-            for (int i = 0; i < messageHandlerCount; i++)
-            {
-                new Thread(() =>
-                {
-                    while(true)
-                    {
-                        HandleMessageIntern(inputQueue.Take());
-                    }
-                }).Start();
-            }
-
-            for (int i = 0; i < commandHandlerCount; i++)
-            {
-                new Thread(() =>
-                {
-                    while (true)
-                    {
-                        commandQueue.Take().Execute();
-                    }
-                }).Start();
-            }
         }
 
+        /// <summary>
+        /// converts the message into a ChatMessage and sends it to HandleMessage
+        /// </summary>
+        /// <param name="message">raw message from twitch chat</param>
         private void HandleMessageIntern(string message)
         {
             if (message.IsChatMessage())
@@ -134,11 +175,23 @@ namespace TwitchBot
             }
         }
 
+        /// <summary>
+        /// handles the ChatMessage and returns a Command
+        /// meant to be overridden
+        /// </summary>
+        /// <param name="cm">ChatMessage</param>
+        /// <returns>a Command to be executed or null if nothing is to be executed</returns>
         public virtual Command HandleMessage(ChatMessage cm)
         {
             return new ResponseCommand(this, $"Hi {cm.Writer}, you wrote: {cm.Message}");
         }
 
+        /// <summary>
+        /// send message to twitch to join a channel
+        /// does NOT check if channel name is valid
+        /// </summary>
+        /// <param name="channelName">channel name</param>
+        /// <returns>true if channel is not empty string</returns>
         public bool JoinChannel(string channelName)
         {
             this.channelName = channelName;
@@ -154,13 +207,19 @@ namespace TwitchBot
             }
         }
 
-
+        /// <summary>
+        /// sends message to twitch to leave the current channel
+        /// </summary>
         public void LeaveChannel()
         {
             outputQueue.Add("PART #" + channelName);
             this.channelName = "";
         }
 
+        /// <summary>
+        /// converts and adds a message to be added to the outputQueue
+        /// </summary>
+        /// <param name="message">raw message to be sent</param>
         public void SendChatMessage(string message)
         {
             outputQueue.Add(message.convertForTwitch(userName, channelName));
