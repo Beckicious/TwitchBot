50b643ec52d8d4d58ab282ac2eb952240388ceb9
Simon Beck <simon.beck@students.fhnw.ch>
2016-05-23 08:46:13 +0200

    refactored ChatMessage

--- TwitchBot/ChatMessage.cs
+++ TwitchBot/ChatMessage.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TwitchBot
+﻿namespace TwitchBot
 {
+    /// <summary>
+    /// Object that represents a message in the chat with its user.
+    /// </summary>
     public class ChatMessage
     {
+        /// <summary>
+        /// writer of the message
+        /// </summary>
         public string Writer { get; set; }
+
+        /// <summary>
+        /// message that has been written in chat
+        /// </summary>
         public string Message { get; set; }
 
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="writer">writer</param>
+        /// <param name="message">message</param>
         public ChatMessage(string writer, string message)
         {
             this.Writer = writer;
