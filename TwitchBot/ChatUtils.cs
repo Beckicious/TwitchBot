053ed86410182832ac1c9f3b5c39a39fb2228115
Simon Beck <simon.beck@students.fhnw.ch>
2016-05-23 08:56:04 +0200

    refactored ChatUtils

--- TwitchBot/ChatUtils.cs
+++ TwitchBot/ChatUtils.cs
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace TwitchBot
 {
+    /// <summary>
+    /// Utility methods
+    /// </summary>
     public static class ChatUtils
     {
+        /// <summary>
+        /// parses a message from twitch to a ChatMessage
+        /// </summary>
+        /// <param name="rawMessage">raw message as recieved from twitch</param>
+        /// <returns>ChatMessage</returns>
         static public ChatMessage ParseMessageToChatMessage(this string rawMessage)
         {
 
@@ -22,16 +30,23 @@ namespace TwitchBot
             return new ChatMessage(writer, message);
         }
 
+        /// <summary>
+        /// checks if a recieved message is a chat message
+        /// </summary>
+        /// <param name="msg">the message to check</param>
+        /// <returns>true if it's a chat message, false otherwise</returns>
         public static bool IsChatMessage(this string msg)
         {
             return msg.Contains("PRIVMSG");
         }
 
-        public static bool IsBotMessage(this string msg)
-        {
-            return msg.First() == '!';
-        }
-
+        /// <summary>
+        /// converts a message into twitch format
+        /// </summary>
+        /// <param name="message">the message to send</param>
+        /// <param name="userName">the userName of the sender</param>
+        /// <param name="channelName">the channelName to send the message</param>
+        /// <returns>converted string ready to send</returns>
         public static string convertForTwitch(this string message, string userName, string channelName)
         {
             return ":" + userName + "!" + userName + "@" + userName + ".tmi.twitch.tv PRIVMSG #" + channelName + " :" + message;
