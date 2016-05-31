b0964d6b9f98ce80423f3679c10295b8c7bc52c6
Simon Beck <simon.beck@students.fhnw.ch>
2016-05-31 11:46:41 +0200

    added creation of empty file if not found

--- ResponseTwitchBot/Program.cs
+++ ResponseTwitchBot/Program.cs
@@ -19,13 +19,23 @@ namespace ResponseTwitchBot
                 StreamReader reader = new StreamReader(@".\data.txt");
                 string s = reader.ReadLine();
                 Data d = JsonConvert.DeserializeObject<Data>(s);
+                reader.Close();
 
                 ResponseTwitchBot bsbot = new ResponseTwitchBot(d.UserName, d.Token);
                 bsbot.Connect();
                 bsbot.StartConsoleReader();
             } catch
             {
+                var d = new Data("INSERT_TWITCH_BOT_ACCOUNT_NAME_HERE", "INSERT_TWITCH_BOT_TOKEN_HERE");
+                string s = JsonConvert.SerializeObject(d);
+
+                StreamWriter writer = new StreamWriter(@".\data.txt");
+                writer.WriteLine(s);
+                writer.Flush();
+                writer.Close();
+
                 Console.WriteLine("no data.txt file found");
+                Console.WriteLine("Empty data.txt file created, please replace the values.");
                 Console.ReadLine();
             }
 
